b17899ca3f74942926939657621e4ba37b45b86d
AllSafeCybercurity <skull32273227@gmail.com>
2022-06-29 12:12:57 -0700

    `dox`

--- doxing/dapp/urls.py
+++ doxing/dapp/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('', views.index, name="index"),
+	path('', views.home, name="home"),
+	path('contact.html', views.contact, name="contact"),
+	path('about.html', views.about, name="about"),
+	path('pricing.html', views.pricing, name="pricing"),
+	path('service.html', views.service, name="service"),
+	path('appointment.html', views.appointment, name="appointment"),
 ]
